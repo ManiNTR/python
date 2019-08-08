ece54e8c898222522dc78ce8d58ad93876ad4190
ManiNTR <48146986+ManiNTR@users.noreply.github.com>
2019-08-08 10:25:07 +0530

    Create Length of tuple.py

---
+++ Length of tuple.py
@@ -0,0 +1,4 @@
+values=input("Enter the values separated by comma:")
+tuple1=tuple(values.split(","))
+print("The elements in the tuple are:",tuple1)
+print("The length of the tuple are: ",len(tuple1))
