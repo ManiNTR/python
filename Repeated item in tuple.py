a012adc5b2b46a274b4f3cbcd09e9a9bdc2f1ca9
ManiNTR <48146986+ManiNTR@users.noreply.github.com>
2019-08-08 10:23:30 +0530

    Create Repeated item in tuple.py

---
+++ Repeated item in tuple.py
@@ -0,0 +1,12 @@
+values=input("Enter the values separated by comma:")
+list1=values.split(",")
+tuple1=tuple(list1)
+l=[]
+for i in tuple1:
+    if tuple1.count(i)>1:
+        l.append(i)
+print("The repeated item in tuple are: ")
+print(set(l))
+        
+
+        
