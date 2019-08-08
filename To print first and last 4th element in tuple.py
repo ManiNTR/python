9c8c27eee1754b60e230be82427568cb8df4784e
ManiNTR <48146986+ManiNTR@users.noreply.github.com>
2019-08-08 10:25:39 +0530

    Create To print first and last 4th element in tuple.py

---
+++ To print first and last 4th element in tuple.py
@@ -0,0 +1,8 @@
+values=input("Enter the values separated by comma:")
+tuple1=tuple(values.split(","))
+print(tuple1[3])
+print(tuple1[-4])
+values=input("Enter the values separated by comma:")
+tuple1=tuple(values.split(","))
+print(tuple1[3])
+print(tuple1[-4])
